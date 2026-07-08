5821077eff1a276b5611b22796475b5fd9079065
Raymond Mukonda <Raymondkogen@gmail.com>
2026-07-08 15:19:38 +0200

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello kindly switch on the url to /contacts ');
+    res.send('Hello kindly switch on the url to /users to access my Database ');
 });
 
 router.use('/users', require('./users'))
