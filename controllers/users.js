1fae7570b6a46c83c834cb5449dbac0662424fab
Raymond Mukonda <Raymondkogen@gmail.com>
2026-07-09 19:32:16 +0200

    put post and dlete

--- controllers/users.js
+++ controllers/users.js
@@ -1,35 +1,106 @@
 const mongodb = require('../data/database.js');
-const ObjectId = require('mongodb').ObjectId;
+const { ObjectId } = require('mongodb');
 
+// GET all users
 const getAll = async (req, res) => {
-    const result = await mongodb
-        .getDatabase()
-        .db('cse341-project1')
-        .collection('contacts')
-        .find();
+  try {
+    const db = mongodb.getDatabase().db('cse341-project1');
+    const result = await db.collection('contacts').find();
+    const contacts = await result.toArray();
 
-    result.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts);
-    });
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 };
 
+// GET single user
 const getSingle = async (req, res) => {
+  try {
     const userId = new ObjectId(req.params.id);
+    const db = mongodb.getDatabase().db('cse341-project1');
+    const result = await db.collection('contacts').find({ _id: userId });
+    const contacts = await result.toArray();
 
-    const result = await mongodb
-        .getDatabase()
-        .db('cse341-project1')
-        .collection('contacts')
-        .find({ _id: userId });
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts[0]);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
 
-    result.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0]);
-    });
+// CREATE (POST)
+const createUser = async (req, res) => {
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  try {
+    const db = mongodb.getDatabase().db('cse341-project1');
+    const response = await db.collection('contacts').insertOne(user);
+
+    if (response.acknowledged) {
+      res.status(201).json(response); 
+    } else {
+      res.status(500).json(response.error || 'Error creating user.');
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+// UPDATE (PUT)
+const updateUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  try {
+    const db = mongodb.getDatabase().db('cse341-project1');
+    const response = await db.collection('contacts').replaceOne({ _id: userId }, user);
+
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'Error updating user.');
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+// DELETE
+const deleteUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+
+  try {
+    const db = mongodb.getDatabase().db('cse341-project1');
+    const response = await db.collection('contacts').deleteOne({ _id: userId });
+
+    if (response.deletedCount > 0) {
+      res.status(204).send(); 
+    } else {
+      res.status(500).json(response.error || 'Error deleting user.');
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 };
 
 module.exports = {
-    getAll,
-    getSingle
+  getAll,
+  getSingle,
+  createUser,
+  updateUser,
+  deleteUser
 };
